f30815e17da64dc14bbf7288402288436eb7b467
NathanDanziger <NateMakesGames@gmail.com>
2024-10-19 11:24:29 -0400

    Created new project in Unity 6000.0.23f1. Added simple top-down character controller.

---
+++ Assets/_WerewolfShopper/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Camera cam;
+    private Vector2 movement;
+
+    void Update()
+    {
+        // Get input from WASD or arrow keys
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement to Rigidbody
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
