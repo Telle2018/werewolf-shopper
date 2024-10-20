6ddb7b39764fd97905302d42fed8147dd09c6160
Nathan Danziger <natemakesgames@gmail.com>
2024-10-20 15:24:25 -0400

    Progress on grocery list during wolf mode.

--- Assets/_WerewolfShopper/Scripts/DayNightController.cs
+++ Assets/_WerewolfShopper/Scripts/DayNightController.cs
@@ -9,6 +9,7 @@ public class DayNightController : MonoBehaviour
     public bool IsDay { get; private set; }
 
     [SerializeField] GameObject nightPanel;
+    [SerializeField] GameObject groceryList;
     [SerializeField] PoliceSpawner policeSpawner;
 
     private void Awake()
@@ -29,6 +30,7 @@ public class DayNightController : MonoBehaviour
     private void Start()
     {
         SoundManager.Instance.PlayDayMusic();
+        groceryList.SetActive(true);
     }
 
     public void TransitionToNight()
@@ -40,6 +42,8 @@ public class DayNightController : MonoBehaviour
 
         // Red panel overlay
         nightPanel.SetActive(true);
+        
+        groceryList.SetActive(false);
 
         // Change player image to werewolf
         PlayerController.Instance.BecomeWolf();
@@ -60,6 +64,8 @@ public class DayNightController : MonoBehaviour
 
         // Remove red panel overlay
         nightPanel.SetActive(false);
+        
+        groceryList.SetActive(true);
 
         // Change player image to human
         PlayerController.Instance.BecomeHuman();
